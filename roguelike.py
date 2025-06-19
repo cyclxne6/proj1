0b96fc2a29362cc51f9570c13f529ebe5139cab2
cyclxne6 <davidshooter3@gmail.com>
2025-06-19 18:56:57 +0100

    feature update
    added main menu

--- roguelike.py
+++ roguelike.py
@@ -1,7 +1,6 @@
 #TODO
 #add dash (200 pixels in one direction, 1.5s cooldown)
-#add hp
-#add stats (defense (x% reduction in damage))
+
 #add enemies (melee (basic, dasher), ranged(bow, blaster))
 #add attacks
 #add player sprite
@@ -27,54 +26,144 @@
 import pygame
 from pygame.locals import *
 import sys
+import button
+import random
 pygame.init()
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 900
-#im not sure why i cant just write 1600 and 900 in the screen and background variables but i trust codingwithruss
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-background=pygame.image.load('BHBackgroundtest.png')
+pygame.display.set_caption('Roguelike Experiment')
+#images
+background=pygame.image.load('RogueBackgroundGRASS.png')
 background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+start = pygame.image.load('STARTbutton.png').convert_alpha()
+exit = pygame.image.load('EXITbutton.png').convert_alpha()
 
 clock = pygame.time.Clock()
 
-delta_time = 0.1
+
+#button instances
+
+start_button = button.Button(540, 250, start, 0.5)
+exit_button = button.Button(540, 400, exit, 0.5)
+
+Menu = True
+
 x=0
+y=0
+HP = 100
+delta_time = 0.1
+#create him
+player = pygame.Rect(610, 650, 50, 50)
 
-player = pygame.Rect(800, 450, 50, 50)
-font = pygame.font.Font(None, size=30)
-HP = 100 #unused atm
+#fonts here
+fpsfont = pygame.font.Font(None, size=30)
+statfont = pygame.font.Font(None, size=50)
+deathfont = pygame.font.Font(None, size=100)
 
+#stats
+STRENGTH = 0 # +1 strength = +1 melee damage
+DEFENSE = 0 # +1 defense = +1% damage reduction
+RANGED = 0 # +1 ranged + +1 ranged damage
+SPEED = 10 # +1 walkspeed = +1 pixel/second
+
+GetAttacked = False
+MeleeAttackedEnemy = False
+RangeAttackedEnemy = False
+
+Weapon = "Stick"
+
+
+
+
+EnemyAttackDMG = 1
+#loop
 running = True
 while running:
     #need main menu here
+    if Menu == True:
+        screen.fill((52, 52, 52))
 
+    if start_button.draw(screen):
+        Menu = False
+        screen.blit(background, (0, 0))
+    
+    if exit_button.draw(screen):
+        running = False
+
+    if Menu == False:
+        start_button = button.Button(0, 0, start, 0)
+        exit_button = button.Button(0, 0, exit, 0)
     #menu end
-    screen.blit(background, (0, 0))
+    if Menu == False:
+        screen.blit(background, (0, 0))
     def fps_counter():
         fps = str(int(clock.get_fps()))
-        fps_t = font.render(fps , 1, pygame.Color("YELLOW"))
+        fps_t = fpsfont.render(fps , 1, pygame.Color("YELLOW"))
         screen.blit(fps_t,(0,0))
+    
     fps_counter()
 
+    def HP_counter():
+        HPrender = str(HP)
+        HP_t = statfont.render("HP: "+HPrender, 1, pygame.Color("RED"))
+        screen.blit(HP_t, (0,850))
+    if Menu == False:
+        HP_counter()
 
+    
+    if Weapon == "Stick":
+        MeleeDMG = 1
+        RangedDMG = 0
     clock.tick(240) # i have 240hz so it works
 
-    pygame.draw.rect(screen, (255, 255, 255), player)
+    def game_over():
+        gameover = str("You died.")
+        rendergameover =deathfont.render(gameover, 1, pygame.Color("RED"))
+        rect = rendergameover.get_rect(center=(SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+        screen.fill((0,0,0))
+        screen.blit(rendergameover, rect)
+        
+    
+    #getting attacked
+    if GetAttacked == True:
+        HP -= EnemyAttackDMG * (DEFENSE/100)
+        GetAttacked = False
+    #attacking something
+    if MeleeAttackedEnemy:
+        Damage = MeleeDMG + STRENGTH
+    if RangeAttackedEnemy:
+        Damage = RangedDMG + RANGED
+
+
+    if Menu == False:
+        pygame.draw.rect(screen, (255, 255, 255), player)
+    #consistent movement across fps drops
     x += 50 * delta_time
+    #cant move if dead, will use for death animation later on
+    if HP > 0:
+        key =pygame.key.get_pressed()
 
-    key =pygame.key.get_pressed()
-    if key [pygame.K_a] == True:
-        player.move_ip(-10,0)
-    if key [pygame.K_d] == True:
-        player.move_ip(10,0)
-    if key [pygame.K_w] == True:
-        player.move_ip(0,-10)
-    if key [pygame.K_s] == True:
-        player.move_ip(0,10)
+        if key [pygame.K_w] or key [pygame.K_UP]:
+            player.move_ip(0,-SPEED)
+        if key [pygame.K_a] or key [pygame.K_LEFT]:
+            player.move_ip(-SPEED,0)
+        if key [pygame.K_s] or key [pygame.K_DOWN]:            
+            player.move_ip(0,SPEED)
+        if key [pygame.K_d] or key [pygame.K_RIGHT]:
+            player.move_ip(SPEED,0)
+        
 
-
+        #gameover testing
+        if key [pygame.K_ESCAPE] == True:
+            HP = -1
+            HPrender = HP
+    else:
+        game_over()
+    
 
 
     for event in pygame.event.get():
@@ -84,6 +173,6 @@ while running:
     pygame.display.flip()
     delta_time = clock.tick(60) / 1000
     delta_time = max(0.001, 0.1, delta_time)
-
+    
 pygame.quit()
 sys.exit()
